40e4e93d75cc1ec8d11150fa22f409f8c340d68d
Ajiteru Dolapo <83882048+Dol-Desh@users.noreply.github.com>
2025-11-24 19:56:39 +0100

    backend additions

--- backend/models/tourModel.js
+++ backend/models/tourModel.js
@@ -76,16 +76,6 @@ tourSchema.pre("save", function (next) {
   next();
 });
 
-// tourSchema.pre("save", function (next) {
-//   console.log("Will save document...");
-//   next();
-// });
-
-// tourSchema.post("save", function (doc, next) {
-//   console.log(doc);
-//   next();
-// });
-
 //Query middleware
 // tourSchema.pre("find", function (next) {
 tourSchema.pre(/^find/, function (next) {
